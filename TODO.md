21e8b9bb2efd09cf955659a40bcdc20be595e72f
David Park <dpjungmin@gmail.com>
2025-09-22 21:58:08 -0700

    generate units from a text file

--- TODO.md
+++ TODO.md
@@ -1,12 +1,10 @@
 # TODO
 
-- impl approx for Quantity comparison
-- codegen units from a config file
+- add docs
+- add doc-tests
 - add binary prefixes (i.e., kibi, mebi, gibi, etc.)
 - add more SI derived units
 - add common mathematical operations (`abs`, `powi`, `sqrt`)
 - add feature flags for conditional compilation
   - unit categories (electrical, mechanical, etc.)
   - prefix sets (common vs. all)
-- add docs
-- add doc-tests
--- src/unit/mod.rs
+++ src/unit/mod.rs
@@ -31,34 +31,4 @@ impl<D: Dimensions> Unit<D> {
     }
 }
 
-define_units! {
-    // base units
-    second (s): Time,
-    meter (m): Length,
-    gram (g): Mass, // kilogram is the base unit
-    ampere (A): ElectricCurrent,
-    kelvin (K): ThermodynamicTemperature,
-    mole (mol): AmountOfSubstance,
-    candela (cd): LuminousIntensity,
-
-    // common derived units
-    hertz (Hz): Frequency,
-    newton (N): Force,
-    joule (J): Energy,
-    watt (W): Power,
-    pascal (Pa): Pressure,
-
-    // electrical units
-    volt (V): Voltage,
-    ohms (Ohm): Resistance,
-    siemens (S): Conductance,
-    coulomb (C): ElectricCharge,
-    farad (F): Capacitance,
-    henry (H): Inductance,
-    tesla (T): MagneticFluxDensity,
-    weber (Wb): MagneticFlux,
-
-    // kinematic units
-    meter_per_second (mps): Velocity,
-    meter_per_second_squared (mps2): Acceleration,
-}
+include!(concat!(env!("OUT_DIR"), "/units_generated.rs"));
